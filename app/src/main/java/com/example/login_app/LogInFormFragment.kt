a39f2b12273f8718252e0b5232e4333d215dff16
Tomás Aranda <tomas.aranda.95@gmail.com>
2021-11-18 15:44:30 -0300

    Add navigation to users list with arguments

--- app/src/main/java/com/example/login_app/LogInFormFragment.kt
+++ app/src/main/java/com/example/login_app/LogInFormFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
@@ -26,8 +27,25 @@ class LogInFormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.log_button).setOnClickListener {
-            val action = LogInFormFragmentDirections.actionLogInFormFragmentToUserFragment()
-            findNavController().navigate(action)
+            // Get EditText values
+            val username = view.findViewById<EditText>(R.id.editTextTextUsername).text.toString()
+            val password = view.findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+
+            val userToLog = UserContent.UserItem(name = username, password = password)
+            try {
+                // Log user
+                when (args.loginType) {
+                    LoginType.LOG_IN -> UserContent.logUserIn(userToLog)
+                    LoginType.SIGN_UP -> UserContent.registerNewUser(userToLog)
+                }
+
+                // Navigate
+                val action = LogInFormFragmentDirections.actionLogInFormFragmentToUserFragment(userToLog.name)
+                findNavController().navigate(action)
+            } catch (e: Exception) {
+                // TODO: Show dialogAlert
+                Toast.makeText(context, "Error ${e.message}", Toast.LENGTH_LONG).show()
+            }
         }
 
         val formTitle = view.findViewById<TextView>(R.id.logFormTitle)
--- app/src/main/java/com/example/login_app/UserFragment.kt
+++ app/src/main/java/com/example/login_app/UserFragment.kt
@@ -1,5 +1,6 @@
 package com.example.login_app
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,11 +8,41 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.navArgs
 
 /**
  * A fragment representing a list of Users.
  */
 class UserFragment : Fragment() {
+    private val args: UserFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//
+//        // This callback will only be called when MyFragment is at least Started.
+//        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+//            OnBackPressedCallback(true) {
+//                // Handle the back button event
+//                val builder = AlertDialog.Builder(getActivity())
+//
+//                builder.setTitle("Save Or Not")
+//                builder.setMessage("Do you want to save this? ")
+//                builder.setPositiveButton("Save", DialogInterface.OnClickListener()) {
+//                    onClick(dialog, id) {
+//                        // Save your content
+//                        save()
+//                        // Then pop
+//                        NavHostFragment.findNavController(MyFragment.this).popBackStack()
+//                    }
+//                    builder.setNegativeButton("Discard",null)
+//                    builder.show()
+//                })
+//            }
+//        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +53,9 @@ class UserFragment : Fragment() {
         val rv: RecyclerView = view.findViewById(R.id.list)
         rv.layoutManager = LinearLayoutManager(context)
         rv.adapter = UsersAdapter(UserContent.listOfUsers)
+
+        val title = view.findViewById<TextView>(R.id.list_title)
+        title.text = args.loggedUserName
 
         return view
     }
